47847be99b0dad39c3b75fcece61b2e10af69f28
yudhisthereal <yudhisthereal@gmail.com>
2025-12-30 11:29:58 +0700

    update gitignore

--- tools/wifi_connect.py
+++
@@ -1,9 +0,0 @@
-from maix import network, err
-
-def connect_wifi(ssid, password):
-    w = network.wifi.Wifi()
-    e = w.connect(ssid, password, wait=True, timeout=60)
-    err.check_raise(e, "connect wifi failed")
-    ip = w.get_ip()
-    print("Connect success, got ip:", ip)
-    return ip
